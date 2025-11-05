fd374896432e584e6ed804aefe3a504d84933907
Miguel Alfonso Principe <miguel.principe@mail.utoronto.ca>
2025-11-05 00:21:36 -0500

    changed temp table according to session 3

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -133,13 +133,16 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
-CREATE TEMPORARY TABLE temp_new_vendor AS SELECT * FROM vendor;
+DROP TABLE IF EXISTS temp.new_vendor;
 
-INSERT INTO temp_new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+CREATE TABLE temp.new_vendor AS 
+SELECT * FROM vendor;
+
+INSERT INTO new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
 VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
 
 SELECT *
-FROM temp_new_vendor;
+FROM new_vendor;
 --check old table
 SELECT *
 FROM vendor;
